b993d1896233443e1a8dc12ba1daa6cd22191229
Jude Gao <jude.gao@vercel.com>
2025-01-01 01:33:12 -0500

    inline css

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     dynamicIO: true,
-    ppr: true
-  }
+    ppr: true,
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
